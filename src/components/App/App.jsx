17bd1a45e0d97c1876365d07837b07413a7268fe
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-09-12 00:48:03 +0300

    init files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+import { Container } from './App.styled';
+
+class App extends Component {
+  render() {
+    return <Container>Hello</Container>;
+  }
+}
+
+export default App;
---
+++ src/components/App/App.styled.js
@@ -0,0 +1,16 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  width: 500px;
+
+  color: #010101;
+  background-color: transparent;
+  border: 1px solid black;
+  border-radius: 4px;
+
+  padding: 20px;
+`;
